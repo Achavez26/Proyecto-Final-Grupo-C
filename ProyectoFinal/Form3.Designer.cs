0df7e4b03ba4fa5a6dc5e2c6587335365a1b7c97
Karen Nahomi Zurita Lopez <nahomi.zurita@unach.edu.ec>
2024-02-19 00:06:54 -0500

    Cambio de lugar de botones, agregación de fonde de consola y edición

--- ProyectoFinal/Form3.Designer.cs
+++ ProyectoFinal/Form3.Designer.cs
@@ -42,9 +42,9 @@ namespace ProyectoFinal
             // btnVer
             // 
             this.btnVer.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnVer.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVer.Font = new System.Drawing.Font("Trebuchet MS", 9.8F, System.Drawing.FontStyle.Bold);
             this.btnVer.Location = new System.Drawing.Point(320, 103);
-            this.btnVer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnVer.Margin = new System.Windows.Forms.Padding(2);
             this.btnVer.Name = "btnVer";
             this.btnVer.Size = new System.Drawing.Size(78, 28);
             this.btnVer.TabIndex = 0;
@@ -55,11 +55,12 @@ namespace ProyectoFinal
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(162, 21);
+            this.label1.BackColor = System.Drawing.Color.Azure;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 24);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(437, 36);
+            this.label1.Size = new System.Drawing.Size(500, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "Listado Estudiantes Aprobados";
             // 
@@ -67,19 +68,19 @@ namespace ProyectoFinal
             // 
             this.dgvAprovados.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dgvAprovados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAprovados.Location = new System.Drawing.Point(94, 157);
-            this.dgvAprovados.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvAprovados.Location = new System.Drawing.Point(146, 177);
+            this.dgvAprovados.Margin = new System.Windows.Forms.Padding(2);
             this.dgvAprovados.Name = "dgvAprovados";
             this.dgvAprovados.RowHeadersWidth = 51;
             this.dgvAprovados.RowTemplate.Height = 24;
-            this.dgvAprovados.Size = new System.Drawing.Size(542, 186);
+            this.dgvAprovados.Size = new System.Drawing.Size(542, 185);
             this.dgvAprovados.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(110, 74);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(134, 77);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(83, 67);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -89,9 +90,9 @@ namespace ProyectoFinal
             // btnSalir
             // 
             this.btnSalir.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnSalir.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Font = new System.Drawing.Font("Trebuchet MS", 9.8F, System.Drawing.FontStyle.Bold);
             this.btnSalir.Location = new System.Drawing.Point(457, 103);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(78, 28);
             this.btnSalir.TabIndex = 4;
@@ -104,13 +105,16 @@ namespace ProyectoFinal
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(733, 389);
+            this.BackgroundImage = global::ProyectoFinal.Properties.Resources.aprobados;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(754, 404);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dgvAprovados);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVer);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
--- ProyectoFinal/Properties/Resources.Designer.cs
+++ ProyectoFinal/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace ProyectoFinal.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap aprobados {
+            get {
+                object obj = ResourceManager.GetObject("aprobados", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PROYECTO {
             get {
                 object obj = ResourceManager.GetObject("PROYECTO", resourceCulture);

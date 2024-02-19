b2c9099b1088eaee438145f276486016dfec379c
Carlos Eduardo Criollo Cantos <carlos.criollo@unach.edu.ec>
2024-02-18 22:33:59 -0500

    Modificación Forms 1

--- ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/Form1.Designer.cs
@@ -45,9 +45,11 @@ namespace ProyectoFinal
             this.btnAprovadosL = new System.Windows.Forms.Button();
             this.btnSuspensoL = new System.Windows.Forms.Button();
             this.mtxtPromedio = new System.Windows.Forms.MaskedTextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnLimpiar = new System.Windows.Forms.Button();
             this.lklblSiguiente = new System.Windows.Forms.LinkLabel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,7 +59,7 @@ namespace ProyectoFinal
             this.label7.Font = new System.Drawing.Font("Ravie", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(237, 97);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 17);
+            this.label7.Size = new System.Drawing.Size(91, 19);
             this.label7.TabIndex = 0;
             this.label7.Text = "Nombres";
             // 
@@ -67,7 +69,7 @@ namespace ProyectoFinal
             this.label8.Font = new System.Drawing.Font("Ravie", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(237, 137);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(80, 17);
+            this.label8.Size = new System.Drawing.Size(94, 19);
             this.label8.TabIndex = 1;
             this.label8.Text = "Apellidos";
             // 
@@ -77,7 +79,7 @@ namespace ProyectoFinal
             this.label9.Font = new System.Drawing.Font("Ravie", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(237, 176);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(54, 17);
+            this.label9.Size = new System.Drawing.Size(63, 19);
             this.label9.TabIndex = 2;
             this.label9.Text = "Curso";
             // 
@@ -87,7 +89,7 @@ namespace ProyectoFinal
             this.label10.Font = new System.Drawing.Font("Ravie", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(237, 221);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(75, 17);
+            this.label10.Size = new System.Drawing.Size(88, 19);
             this.label10.TabIndex = 3;
             this.label10.Text = "Paralelo";
             // 
@@ -97,7 +99,7 @@ namespace ProyectoFinal
             this.label11.Font = new System.Drawing.Font("Ravie", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.Location = new System.Drawing.Point(237, 255);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(83, 17);
+            this.label11.Size = new System.Drawing.Size(97, 19);
             this.label11.TabIndex = 4;
             this.label11.Text = "Promedio";
             // 
@@ -105,28 +107,28 @@ namespace ProyectoFinal
             // 
             this.txtNombreL.Location = new System.Drawing.Point(372, 97);
             this.txtNombreL.Name = "txtNombreL";
-            this.txtNombreL.Size = new System.Drawing.Size(182, 20);
+            this.txtNombreL.Size = new System.Drawing.Size(182, 22);
             this.txtNombreL.TabIndex = 6;
             // 
             // txtApellidoL
             // 
             this.txtApellidoL.Location = new System.Drawing.Point(372, 137);
             this.txtApellidoL.Name = "txtApellidoL";
-            this.txtApellidoL.Size = new System.Drawing.Size(182, 20);
+            this.txtApellidoL.Size = new System.Drawing.Size(182, 22);
             this.txtApellidoL.TabIndex = 7;
             // 
             // txtCursoL
             // 
             this.txtCursoL.Location = new System.Drawing.Point(372, 176);
             this.txtCursoL.Name = "txtCursoL";
-            this.txtCursoL.Size = new System.Drawing.Size(182, 20);
+            this.txtCursoL.Size = new System.Drawing.Size(182, 22);
             this.txtCursoL.TabIndex = 8;
             // 
             // txtParaleloL
             // 
             this.txtParaleloL.Location = new System.Drawing.Point(372, 216);
             this.txtParaleloL.Name = "txtParaleloL";
-            this.txtParaleloL.Size = new System.Drawing.Size(182, 20);
+            this.txtParaleloL.Size = new System.Drawing.Size(182, 22);
             this.txtParaleloL.TabIndex = 9;
             // 
             // label12
@@ -135,7 +137,7 @@ namespace ProyectoFinal
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(253, 24);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(295, 36);
+            this.label12.Size = new System.Drawing.Size(364, 42);
             this.label12.TabIndex = 10;
             this.label12.Text = "Listado Estudiantes";
             // 
@@ -192,18 +194,8 @@ namespace ProyectoFinal
             this.mtxtPromedio.Location = new System.Drawing.Point(372, 255);
             this.mtxtPromedio.Mask = "99";
             this.mtxtPromedio.Name = "mtxtPromedio";
-            this.mtxtPromedio.Size = new System.Drawing.Size(63, 20);
+            this.mtxtPromedio.Size = new System.Drawing.Size(63, 22);
             this.mtxtPromedio.TabIndex = 15;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(51, 56);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(102, 83);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 16;
-            this.pictureBox1.TabStop = false;
             // 
             // btnLimpiar
             // 
@@ -223,11 +215,31 @@ namespace ProyectoFinal
             this.lklblSiguiente.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.lklblSiguiente.Location = new System.Drawing.Point(694, 42);
             this.lklblSiguiente.Name = "lklblSiguiente";
-            this.lklblSiguiente.Size = new System.Drawing.Size(51, 13);
+            this.lklblSiguiente.Size = new System.Drawing.Size(63, 16);
             this.lklblSiguiente.TabIndex = 18;
             this.lklblSiguiente.TabStop = true;
             this.lklblSiguiente.Text = "Siguiente";
             this.lklblSiguiente.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lklblSiguiente_LinkClicked);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProyectoFinal.Properties.Resources.WhatsApp_Image_2024_02_18_at_10_30_09_PM;
+            this.pictureBox2.Location = new System.Drawing.Point(-6, -11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(805, 410);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(51, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -251,9 +263,11 @@ namespace ProyectoFinal
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             this.TransparencyKey = System.Drawing.Color.DimGray;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -295,6 +309,7 @@ namespace ProyectoFinal
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.LinkLabel lklblSiguiente;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 

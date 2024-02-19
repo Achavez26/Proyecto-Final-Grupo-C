e5aa92c5e5b925747fd7f75e0eca80781de72874
Karen Nahomi Zurita Lopez <nahomi.zurita@unach.edu.ec>
2024-02-19 00:36:30 -0500

    Form5:Cambio de apariencia de consola y arreglos de botos y letras con sus respectivos tamaños

--- ProyectoFinal/Form5.Designer.cs
+++ ProyectoFinal/Form5.Designer.cs
@@ -43,28 +43,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 46);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Rockwell", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(37, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 16);
+            this.label1.Size = new System.Drawing.Size(131, 14);
             this.label1.TabIndex = 0;
             this.label1.Text = "CONTAR PROMEDIOS";
             // 
             // mtxtPromedio
             // 
-            this.mtxtPromedio.Location = new System.Drawing.Point(229, 39);
-            this.mtxtPromedio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mtxtPromedio.Location = new System.Drawing.Point(208, 32);
             this.mtxtPromedio.Mask = "00";
             this.mtxtPromedio.Name = "mtxtPromedio";
-            this.mtxtPromedio.Size = new System.Drawing.Size(132, 22);
+            this.mtxtPromedio.Size = new System.Drawing.Size(100, 20);
             this.mtxtPromedio.TabIndex = 1;
             // 
             // btnContar
             // 
-            this.btnContar.Location = new System.Drawing.Point(399, 34);
-            this.btnContar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnContar.Font = new System.Drawing.Font("Stencil", 10F);
+            this.btnContar.Location = new System.Drawing.Point(355, 28);
             this.btnContar.Name = "btnContar";
-            this.btnContar.Size = new System.Drawing.Size(100, 28);
+            this.btnContar.Size = new System.Drawing.Size(99, 42);
             this.btnContar.TabIndex = 2;
             this.btnContar.Text = "Contar";
             this.btnContar.UseVisualStyleBackColor = true;
@@ -73,27 +72,26 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 130);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Rockwell", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(173, 16);
+            this.label2.Size = new System.Drawing.Size(142, 14);
             this.label2.TabIndex = 3;
             this.label2.Text = "BUSCAR POR SEMESTRE";
             // 
             // txtBuscarSe
             // 
-            this.txtBuscarSe.Location = new System.Drawing.Point(229, 130);
-            this.txtBuscarSe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBuscarSe.Location = new System.Drawing.Point(208, 106);
             this.txtBuscarSe.Name = "txtBuscarSe";
-            this.txtBuscarSe.Size = new System.Drawing.Size(132, 22);
+            this.txtBuscarSe.Size = new System.Drawing.Size(100, 20);
             this.txtBuscarSe.TabIndex = 4;
             // 
             // btnMostrarSe
             // 
-            this.btnMostrarSe.Location = new System.Drawing.Point(399, 130);
-            this.btnMostrarSe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMostrarSe.Font = new System.Drawing.Font("Stencil", 10F);
+            this.btnMostrarSe.Location = new System.Drawing.Point(355, 106);
             this.btnMostrarSe.Name = "btnMostrarSe";
-            this.btnMostrarSe.Size = new System.Drawing.Size(100, 28);
+            this.btnMostrarSe.Size = new System.Drawing.Size(99, 42);
             this.btnMostrarSe.TabIndex = 5;
             this.btnMostrarSe.Text = "Mostrar ";
             this.btnMostrarSe.UseVisualStyleBackColor = true;
@@ -103,30 +101,32 @@
             // 
             this.dvgBuscarSe.BackgroundColor = System.Drawing.SystemColors.ControlLight;
             this.dvgBuscarSe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dvgBuscarSe.Location = new System.Drawing.Point(39, 192);
-            this.dvgBuscarSe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dvgBuscarSe.Location = new System.Drawing.Point(29, 170);
             this.dvgBuscarSe.Name = "dvgBuscarSe";
             this.dvgBuscarSe.RowHeadersWidth = 51;
-            this.dvgBuscarSe.Size = new System.Drawing.Size(852, 219);
+            this.dvgBuscarSe.Size = new System.Drawing.Size(601, 207);
             this.dvgBuscarSe.TabIndex = 6;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.pictureBox1.Image = global::ProyectoFinal.Properties.Resources.WhatsApp_Image_2024_02_18_at_9_29_55_PM;
-            this.pictureBox1.Location = new System.Drawing.Point(702, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(565, 27);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(255, 169);
+            this.pictureBox1.Size = new System.Drawing.Size(153, 110);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.BackgroundImage = global::ProyectoFinal.Properties.Resources.form5;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(839, 416);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dvgBuscarSe);
             this.Controls.Add(this.btnMostrarSe);
@@ -135,7 +135,7 @@
             this.Controls.Add(this.btnContar);
             this.Controls.Add(this.mtxtPromedio);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DoubleBuffered = true;
             this.Name = "Form5";
             this.Text = "Form5";
             ((System.ComponentModel.ISupportInitialize)(this.dvgBuscarSe)).EndInit();
--- ProyectoFinal/Properties/Resources.Designer.cs
+++ ProyectoFinal/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ProyectoFinal.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap form5 {
+            get {
+                object obj = ResourceManager.GetObject("form5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PROYECTO {
             get {
                 object obj = ResourceManager.GetObject("PROYECTO", resourceCulture);
